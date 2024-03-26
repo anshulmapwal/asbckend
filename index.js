c15a6ce2a034de6b4ec6bcd5b21bc2a5e81d538d
Anshul Singh <anshulmapwal@gmail.com>
2024-03-26 20:26:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('anshulmapwal')
+  })
+
+app.get('/login', (req, res) => {
+  res.send('<h1>loginanshulsinghmapwal</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
